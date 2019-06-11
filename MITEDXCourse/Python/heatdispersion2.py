81f51cf36807b13d15652863beb91858b8de81c0
Prajeetpss@gmail.com <Prajeetpss@gmail.com>
2019-06-10 19:35:12 -0500

    Added cipher 2 and Card

--- MITEDXCourse/Python/heatdispersion2.py
+++ MITEDXCourse/Python/heatdispersion2.py
@@ -10,37 +10,70 @@ import numpy as np
 
 def evolve(u):
     temp = np.copy(u)
-    mainMatrix = temp[1:-1, 1:-1]
-    print(mainMatrix)
-    horzMiddle = int(len(u[0]) /2)
-    vertMiddle = int(len(u)/2)
-
-
-    print(temp[0:-vertMiddle+1,1:-1])
-    mainMatrix = mainMatrix +  0.25*temp[vertMiddle:,1:-1];
-    mainMatrix = mainMatrix +  0.25*temp[1:-1,0:horzMiddle+1];
-    mainMatrix = mainMatrix +  0.25*temp[1:-1,horzMiddle:];
-    mainMatrix = mainMatrix +  0.25*temp[0:vertMiddle+1,1:-1] 
-    temp[1:-1,1:-1] = mainMatrix
+    mainMatrix = temp[1:-1,1:-1]
+    mainMatrix = mainMatrix + 1/4*temp[1:-1, :-2]#Left Border
+    mainMatrix = mainMatrix + 1/4*temp[1:-1, 2:]#Right Border
+    mainMatrix = mainMatrix + 1/4*temp[0:-2, 1:-1] #Top Border
+    mainMatrix = mainMatrix + 1/4*temp[2:, 1:-1] #Bottom Border
+    mainMatrix = mainMatrix - temp[1:-1, 1:-1]
+    temp[1:-1,1:-1] = mainMatrix    
     print(temp)
     return np.asarray(temp)
+    
+    
 
 
-
-input = np.array([[100, 100, 100, 100, 100],
-                            [100,   0,   0,   0, 100],
-                            [100,   0,   0,   0, 100],
-                            [100,   0,   0,   0, 100],
-                            [100, 100, 100, 100, 100]])
-
+input = np.array([[0.47, 0.26, 0.48, 0.7 ],
+                  [0.95, 0.89, 0.43, 0.92],
+                  [0.85, 0.64, 0.54, 0.98],
+                  [0.17, 0.08, 0.01, 0.48],
+                  [0.98, 0.15, 0.96, 0.42]])
+"""
+Anwser is 
+[0.47  , 0.26  , 0.48  , 0.7   ],
+[0.95  , 0.57  , 0.7075, 0.92  ],
+[0.85  , 0.59  , 0.515 , 0.98  ],
+[0.17  , 0.2425, 0.515 , 0.48  ],
+[0.98  , 0.15  , 0.96  , 0.42  ]
+   """ 
 evolve(input)
 
 
 
 """
+    Copy222
+        temp = np.copy(u)
+    mainMatrix = temp[1:-1, 1:-1]
+    #print(mainMatrix)
+    square = len(temp) == len(temp[0])
+    print(square)
+    horzMiddle = int(len(u[0]) /2)
+    vertMiddle = int(len(u)/2)
+    print(square)
+    if square == True:
+        #print(temp[0:-vertMiddle+1,1:-1])
+        mainMatrix = mainMatrix +  0.25*temp[vertMiddle:,1:-1];
+        mainMatrix = mainMatrix +  0.25*temp[1:-1,0:horzMiddle+1];
+        mainMatrix = mainMatrix +  0.25*temp[1:-1,horzMiddle:];
+        mainMatrix = mainMatrix +  0.25*temp[0:vertMiddle+1,1:-1] 
+        temp[1:-1,1:-1] = mainMatrix
+        print(temp)
+        return np.asarray(temp)
+    else:
+        mainMatrix = mainMatrix +  0.25*temp[vertMiddle+1:,1:-1];
+        mainMatrix = mainMatrix +  0.25*temp[1:-1,0:horzMiddle+1];
+        mainMatrix = mainMatrix +  0.25*temp[1:-1,horzMiddle:];
+        mainMatrix = mainMatrix +  0.25*temp[0:vertMiddle,1:-1] 
+        temp[1:-1,1:-1] = mainMatrix
+        #print(temp)
+        return np.asarray(temp)
+    Copy222
+
+
+
     Copy111
     
-        temp = np.copy(u)
+    temp = np.copy(u)
     temp[1:-1,1] = temp[1:-1,0] + temp[1:-1,1] #left border
     temp[1:-1,-2] = temp[1:-1,-1] + temp[1:-1,-2] #right border
     temp[1,1:-1] = temp[0,1:-1]+temp[1,1:-1]; #top border
